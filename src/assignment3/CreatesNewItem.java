0fc70ec1b9a2c738842efed4f6a002339584fc9c
zhang1995 <zhangtong@utexas.edu>
2016-02-20 17:11:53 -0600

    CretesNewItem class created but not functional

---
+++ src/assignment3/CreatesNewItem.java
@@ -0,0 +1,17 @@
+package assignment3;
+
+public class CreatesNewItem {
+	
+	/**
+	 * this method will creates a new item according to the command line 
+	 * 
+	 * 
+	 * @param transaction
+	 * @return
+	 */
+	public Item generate_new_item(String[] transaction) {
+	
+		return new Item();
+	}
+
+}
