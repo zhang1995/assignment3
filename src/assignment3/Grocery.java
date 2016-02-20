f33c4448b065bb66c71d5bc853dbed706e74b2f4
zhang1995 <zhangtong@utexas.edu>
2016-02-20 14:43:40 -0600

    item, electronic, clothing, and grocery class are ready to test

--- src/assignment3/Grocery.java
+++ src/assignment3/Grocery.java
@@ -12,6 +12,24 @@ public class Grocery extends Item {
 
 	// override calculatePrice() if necessary; Implement print methods as
 	// necessary
+	public double calculatePrice() {
+		double final_price = 0;
+		// shipping cost
+		double shipping_cost = (20 * weight) * quantity;
+		if (is_perishable) {
+			shipping_cost *= 1.2;
+		}
+		// the price including tax
+		double taxfree_price = price * quantity;
+		final_price = taxfree_price + shipping_cost;
+		return final_price;
+	}
+	
+void printItemAttributes() {
+		
+		System.out.format("Item: %s\n" + "Price: %d\n" + "Quantity: %d\n" + "Weight: %d\n" +"Is_Perishable: %b\n" + "Total Price: %d\n", 
+				name, price, quantity, weight, is_perishable, calculatePrice());
+	}
 
 	// Only re-implement stuff you cannot get from the superclass (Item)
 
